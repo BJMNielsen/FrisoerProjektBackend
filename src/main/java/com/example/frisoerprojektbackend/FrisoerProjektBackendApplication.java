fd98470f8f2e259ef9d6f04e19be687ce4765993
benj302g <benj302g@stud.kea.dk>
2023-05-03 10:10:25 +0200

    Init

---
+++ src/main/java/com/example/frisoerprojektbackend/FrisoerProjektBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.frisoerprojektbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrisoerProjektBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrisoerProjektBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/frisoerprojektbackend/FrisoerProjektBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.frisoerprojektbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrisoerProjektBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
