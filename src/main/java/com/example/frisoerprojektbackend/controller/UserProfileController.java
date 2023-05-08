e010251270350c543b997d3c7978ae97737ffdd0
benj302g <benj302g@stud.kea.dk>
2023-05-08 16:02:01 +0200

    email i userprofile service

--- src/main/java/com/example/frisoerprojektbackend/controller/UserProfileController.java
+++ src/main/java/com/example/frisoerprojektbackend/controller/UserProfileController.java
@@ -19,10 +19,28 @@ public class UserProfileController {
         return userProfileService.getUserProfiles();
     }
 
+    @GetMapping("/userprofiles/{id}")
+    public UserProfile getUserProfileById(@PathVariable int id) {
+        return userProfileService.getUserProfileById(id);
+    }
+
     @PostMapping("/userprofile")
     public ResponseEntity<UserProfile> addUserProfile(@RequestBody UserProfile userProfile) {
         return userProfileService.addUserProfile(userProfile);
     }
+
+    @PutMapping("/userprofile")
+    public ResponseEntity<UserProfile> updateUserProfile(@RequestBody UserProfile userProfile) {
+        return userProfileService.updateUserProfile(userProfile);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<UserProfile> deleteUserProfile(@RequestBody UserProfile userProfile) {
+        return userProfileService.deleteUserProfile(userProfile);
+    }
+
+
+
 }
     /*
     // Finder cyclist ud fra id
--- src/main/java/com/example/frisoerprojektbackend/service/UserProfileService.java
+++ src/main/java/com/example/frisoerprojektbackend/service/UserProfileService.java
@@ -1,6 +1,7 @@
 package com.example.frisoerprojektbackend.service;
 
 import com.example.frisoerprojektbackend.exception.ResourceAlreadyExistsException;
+import com.example.frisoerprojektbackend.exception.ResourceNotFoundException;
 import com.example.frisoerprojektbackend.model.UserProfile;
 import com.example.frisoerprojektbackend.repository.UserProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,10 @@ public class UserProfileService {
 
     public List<UserProfile> getUserProfiles() {
         return userProfileRepository.findAll();
+    }
+
+    public UserProfile getUserProfileById(int id) {
+        return userProfileRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Userprofile with id: " + id + " does not exist and could therefore not find any userprofile."));
     }
 
     public ResponseEntity<UserProfile> addUserProfile(UserProfile userProfile) {
@@ -38,5 +43,38 @@ public class UserProfileService {
             UserProfile newUserProfile = userProfileRepository.save(userProfile);
             return new ResponseEntity<>(newUserProfile, HttpStatus.OK);
         }
+    }
+
+    public ResponseEntity<UserProfile> updateUserProfile(UserProfile userProfile) {
+        boolean exist = userProfileRepository.existsById(userProfile.getId());
+        if(!exist) {
+            throw new ResourceNotFoundException("Userprofile with id: " + userProfile.getId() + " could not be found and therefore could not be updated");
+        }
+        // Her tjekker vi om den nye email de gerne vil have allerede eksistere. Den skal være unik
+        boolean emailExists = userProfileRepository.existsUserProfileByEmail(userProfile.getEmail());
+
+        // Vi finder den nuværende profil i databasen og dens id som den ser ud nu i tabellen, ud fra det overførste userProfiles id.
+        UserProfile oldUserProfile = getUserProfileById(userProfile.getId());
+
+        // Her sammenligner vi den gamle profils email med det nye overførste Json objekts email(den opdaterede profil vi får tilsendt fra frontenden, dvs userProfile)
+        boolean sameEmailAsBefore = oldUserProfile.getEmail().equals(userProfile.getEmail());
+
+        // Hvis emailen allerede eksistere, og det ikke er den samme email som du havde før, så thrower vi en exception og user får ikke lov til at update.
+        if (emailExists && !sameEmailAsBefore) {
+            // Tjekker om der allerede eksistere den email brugeren har skrevet. Hvis den existere thrower vi en exception der bliver grebet i vores exception controller og lavet til et responseEntity object, som vores frontend også kan håndtere.
+            throw new ResourceAlreadyExistsException("Userprofile with email: " + userProfile.getEmail() + " already exists, so you cannot use that email");
+        }
+        // Hvis emailen existere og den er den samme som du tidligere havde, så får du lov til at gemme objektet.
+        UserProfile updatedUserProfile = userProfileRepository.save(userProfile);
+        return new ResponseEntity<>(updatedUserProfile, HttpStatus.OK);
+    }
+
+    public ResponseEntity<UserProfile> deleteUserProfile(UserProfile userProfile) {
+        boolean exists = userProfileRepository.existsById(userProfile.getId());
+        if(!exists){
+            throw new ResourceNotFoundException("Userprofile with id: " + userProfile.getId() + " could not be found and therefore could not be deleted");
+        }
+        userProfileRepository.deleteById(userProfile.getId());
+        return new ResponseEntity<>(userProfile, HttpStatus.OK);
     }
 }
