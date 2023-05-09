b74d26e8979372bb2df05e7477d0d05f548aa9b2
benj302g <benj302g@stud.kea.dk>
2023-05-09 14:43:14 +0200

    Småting bare lige lave ttomme klasser.

--- src/main/java/com/example/frisoerprojektbackend/controller/BookedTreatmentController.java
+++ src/main/java/com/example/frisoerprojektbackend/controller/BookedTreatmentController.java
@@ -1,5 +1,10 @@
 package com.example.frisoerprojektbackend.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
 public class BookedTreatmentController {
-    //lars
+
 }
---
+++ src/main/java/com/example/frisoerprojektbackend/service/BookedTreatmentService.java
@@ -0,0 +1,7 @@
+package com.example.frisoerprojektbackend.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookedTreatmentService {
+}
